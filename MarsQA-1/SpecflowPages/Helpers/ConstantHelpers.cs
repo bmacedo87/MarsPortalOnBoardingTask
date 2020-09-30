539661db8feefda6defd9b00ac5a9a9c8209418b
Bruno <bmacedo1987@gmail.com>
2020-10-01 11:33:00 +1300

    last commit

--- MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
+++ MarsQA-1/SpecflowPages/Helpers/ConstantHelpers.cs
@@ -11,13 +11,5 @@ namespace MarsQA_1.Helpers
         //Base Url
         public static string Url = "http://localhost:5000";
 
-        //ScreenshotPath
-        public static string ScreenshotPath = "";
-
-        //ExtentReportsPath
-        public static string ReportsPath = "";
-
-        //ReportXML Path
-        public static string ReportXMLPath = "";
     }
 }
